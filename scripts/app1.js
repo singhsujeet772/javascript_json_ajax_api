7987f6b38d01537f7de7219f539f9647349bdd00
Sujeet <singhsujeet772@gmail.com>
2024-01-17 05:20:15 +0530

    JavaScript Objects Example of how to use Objects in Code get object data in app1.js

---
+++ scripts/app1.js
@@ -0,0 +1,15 @@
+//(3)JavaScript Objects Example of how to use Objects in Code get object data
+
+const obj1 = {
+"first" : "Laurence",
+"last name" : "Svekis",
+"num" : 100,
+"boo" : false,
+"nu" : null,
+"arr" : [],
+"obj" : {}
+};
+console.log(obj1);
+console.log(obj1.first);
+console.log(obj1["first"]);
+console.log(obj1["last name"]);
