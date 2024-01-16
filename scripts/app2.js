9b2177c5a7c4290e0938fed4bfb105bf9411393f
Sujeet <singhsujeet772@gmail.com>
2024-01-17 05:26:42 +0530

    JavaScript Arrays examples of using arrays within code items and data in array in app2.js

---
+++ scripts/app2.js
@@ -0,0 +1,10 @@
+//(4)JavaScript Arrays examples of using arrays within code items and data in array
+
+const arr = ["Laurence", 100, true, null, {
+    "first": "Laurence",
+    "last": "Svekis"
+}, "Laurence", "Laurence", "Laurence"];
+console.log(arr);
+console.log(arr[5]);
+console.log(arr[4]["first"]);
+console.log(arr[4].last);
